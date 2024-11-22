a050601c2a99405267dab76188213fca505c41b0
leticia nobre <leticiavidal2910@gmail.com>
2024-11-22 17:27:18 -0300

    🔧 typeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevelopersModule } from './developers/developers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [DevelopersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, //não usa em código em produção, só para desenvolvimento, em produção usa migration
+    }),
+    
+    DevelopersModule],
   controllers: [AppController],
   providers: [AppService],
 })
